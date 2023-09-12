528be39b45fe13b435582803faa14b16ed2455ba
Wozniak85 <90185200+Wozniak85@users.noreply.github.com>
2023-09-12 09:27:38 +0200

    Add files via upload

---
+++ Project1/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+
+        public delegate void MySampleDelegate(int number);
+        public static class MyDelegate
+    {
+        public static MySampleDelegate mySampleDelegate;
+
+        static void Main(string[] args)
+        {
+            mySampleDelegate += SampleMethod;
+
+            mySampleDelegate?.Invoke(10);
+        }
+
+        public static void SampleMethod(int number)
+        {
+            Console.WriteLine("Podana liczba: " + number);
+        }
+    }
+}
